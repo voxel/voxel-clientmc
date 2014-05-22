3dda63f5e68bcb8fb8c91d3f6ab9382bc342b383
deathcap <deathcap@gmx.co.uk>
2014-05-21 20:49:03 -0700

    Add placeholders for all missing blocks

--- app/blocks.js
+++ app/blocks.js
@@ -19,6 +19,7 @@ BlocksPlugin.prototype.enable = function() {
   this.registry.registerBlock('grass', {texture: ['grass_top', 'dirt', 'grass_side'], hardness:1.0, itemDrop: 'dirt', effectiveTool: 'spade'});
   this.registry.registerBlock('dirt', {texture: 'dirt', hardness:0.75, effectiveTool: 'spade'});
   this.registry.registerBlock('farmland', {texture: 'farmland_dry'});
+  this.registry.registerBlock('mycelium', {texture: ['mycelium_top', 'dirt', 'mycelium_side']});
   this.registry.registerBlock('stone', {displayName: 'Smooth Stone', texture: 'stone', hardness:10.0, itemDrop: 'cobblestone', effectiveTool: 'pickaxe', requiredTool: 'pickaxe'});
   this.registry.registerBlock('bedrock', {texture: 'bedrock'});
   this.registry.registerBlock('waterFlow', {texture: 'water_flow'}); // TODO: animation
@@ -41,14 +42,16 @@ BlocksPlugin.prototype.enable = function() {
   this.registry.registerBlock('cobblestone', {texture: 'cobblestone', hardness:10.0, effectiveTool: 'pickaxe', requiredTool: 'pickaxe'});
   this.registry.registerBlock('brick', {texture: 'brick'});
   this.registry.registerBlock('leavesOak', {displayName: 'Oak Leaves', texture: 'leaves_oak', transparent: true, hardness: 0.1, creativeTab: 'plants'});
+  this.registry.registerBlock('leavesAcacia', {displayName: 'Acacia Leaves', texture: 'leaves_acacia', transparent: true, hardness: 0.1, creativeTab: 'plants'});
   this.registry.registerBlock('logBirch', {texture: ['log_birch_top', 'log_birch_top', 'log_birch'], hardness:2.0, displayName: 'Birch Wood', effectiveTool: 'axe', creativeTab: 'plants'});
+  this.registry.registerBlock('logAcacia', {displayName: 'Acacia Wood', texture: ['log_acacia_top', 'log_acacia_top', 'log_acacia'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'});
 
   this.registry.registerBlock('sponge', {texture: 'sponge'});
   this.registry.registerBlock('glass', {texture: 'glass', transparent: true, hardness: 0.2});
   this.registry.registerBlock('blockLapis', {texture: 'lapis_block'});
   this.registry.registerBlock('sandstone', {texture: 'sandstone_normal'});
 
-  this.registry.registerBlock('wool', {texture: 'wool_colored_white'}); // TODO: metablocks for colors
+  this.registry.registerBlock('wool', {texture: 'wool_colored_white'}); // TODO: metablocks for colors TODO: use voxel-wool..
 
   this.registry.registerBlock('blockRedstone', {texture: 'redstone_block', displayName: 'Block of Redstone'}); // TODO: move to voxel-decorative?
   this.registry.registerBlock('blockEmerald', {texture: 'emerald_block', displayName: 'Block of Emerald'}); // TODO: move to voxel-decorative?
@@ -71,6 +74,18 @@ BlocksPlugin.prototype.enable = function() {
   this.registry.registerBlock('endstone', {texture: 'end_stone'});
   this.registry.registerBlock('lampOff', {texture: 'redstone_lamp_off'});
   this.registry.registerBlock('lampOn', {texture: 'redstone_lamp_on'});
+
+  this.registry.registerBlock('noteblock', {texture: 'noteblock'});
+  this.registry.registerBlock('dispenser', {texture: 'dispenser_front_horizontal'}); // TODO: direction
+  this.registry.registerBlock('dropper', {texture: 'dropper_front_horizontal'}); // TODO: direction
+  this.registry.registerBlock('mushroomBigRed', {texture: 'mushroom_block_skin_red'});
+  this.registry.registerBlock('mushroomBigBrown', {texture: 'mushroom_block_skin_brown'});
+  this.registry.registerBlock('brickNether', {texture: 'nether_brick'});
+  this.registry.registerBlock('endPortalFrame', {texture: ['endframe_top', 'endframe_top', 'endframe_side']});
+  this.registry.registerBlock('command', {texture: 'command_block'});
+  this.registry.registerBlock('clayStainedWhite', {texture: 'hardened_clay_stained_white'});
+  this.registry.registerBlock('clayHardened', {texture: 'hardened_clay'});
+  this.registry.registerBlock('hayBale', {texture: ['hay_block_top', 'hay_block_top', 'hay_block_side']});
 
   this.registry.registerBlock('missing', {texture: 'no_texture', displayName: 'Missing Block'}); // custom texture (TODO: standard location?)
   // TODO: more blocks
