f333aa4ec22df84a4861544455c66189c8ba4106
deathcap <deathcap@gmx.co.uk>
2014-06-21 13:18:59 -0700

    Update demo for voxel-engine#ndarray

--- app/app.js
+++ app/app.js
@@ -3,25 +3,23 @@
 require('voxel-clientmc');
 require('voxel-registry');
 require('voxel-artpacks');
+require('voxel-wireframe');
+require('voxel-chunkborder');
 require('voxel-carry');
 require('voxel-recipes');
-require('voxel-webview');
 require('voxel-workbench');
 require('voxel-furnace');
 require('voxel-chest');
 require('voxel-inventory-hotbar');
 require('voxel-inventory-crafting');
-require('voxel-highlight');
+require('voxel-outline');
 require('voxel-voila');
-require('voxel-player');
 require('voxel-health');
 require('voxel-health-bar');
 require('voxel-health-fall');
 require('voxel-food');
 require('voxel-sfx');
-require('voxel-fly');
 require('voxel-gamemode');
-require('voxel-walk');
 require('voxel-sprint');
 require('voxel-mine');
 require('voxel-harvest');
@@ -32,16 +30,13 @@ require('voxel-wool');
 require('voxel-pumpkin');
 require('voxel-blockdata');
 require('./blocks.js');
-require('voxel-daylight');
 require('voxel-decorative');
 require('voxel-inventory-creative');
 require('voxel-clientmc');
 require('voxel-console');
 require('voxel-commands');
 require('voxel-drop');
-require('voxel-start');
 require('voxel-zen');
-require('voxel-debug');
 require('voxel-plugins-ui');
 require('voxel-keys');
 require('kb-bindings-ui');
@@ -54,10 +49,6 @@ var main = function() {
     'voxel-engine': {
       appendDocument: true,
       exposeGlobal: true,  // for debugging
-
-      texture_modules: [
-        require('voxel-texture-shader'),
-      ],
 
       lightsDisabled: true,
       arrayTypeSize: 2,  // arrayType: Uint16Array
@@ -107,8 +98,9 @@ var main = function() {
     },
     'voxel-registry': {},
     'voxel-artpacks': {},
+    'voxel-wireframe': {},
+    'voxel-chunkborder': {},
     'voxel-recipes': {},
-    'voxel-webview': {onDemand: true},  // disabled by default until https://github.com/deathcap/voxel-webview/issues/3
     'voxel-carry': {inventoryWidth:10, inventoryRows:5},
     'voxel-blockdata': {},
     'voxel-chest': {},
@@ -117,7 +109,6 @@ var main = function() {
     'voxel-pickaxe': {},
     'voxel-wool': {},
     'voxel-pumpkin': {},
-    'voxel-daylight': {ambientColor: 0x888888, directionalColor: 0xffffff},
 
     './blocks.js': {}, // misc inanimate opaque solid blocks
     'voxel-decorative': {},
@@ -127,19 +118,14 @@ var main = function() {
     'voxel-console': {},
     'voxel-commands': {},
     'voxel-drop': {},
-    'voxel-start': {},
     'voxel-zen': {},
 
-
-    'voxel-player': {image: 'player.png', homePosition: [2,14,4], homeRotation: [0,0,0]},
     'voxel-health': {},
     'voxel-health-bar': {},
     'voxel-health-fall': {},
     'voxel-food': {},
     'voxel-sfx': {},
-    'voxel-fly': {flySpeed: 0.8, onDemand: true},
     'voxel-gamemode': {},
-    'voxel-walk': {},
     'voxel-sprint': {},
     'voxel-inventory-hotbar': {inventorySize:10},
     'voxel-inventory-crafting': {},
@@ -154,17 +140,11 @@ var main = function() {
     'voxel-use': {},
     // handles 'break' event from voxel-mine (left-click hold breaks blocks), collects block and adds to inventory
     'voxel-harvest': {},
-    // highlight blocks when you look at them
-    'voxel-highlight': {
-      color:  0xff0000,
-      distance: 8,
-      adjacentActive: function() { return false; },   // don't hold <Ctrl> for block placement (right-click instead, 'reach' plugin) // TODO: not serializable, problem?,
-    },
+    'voxel-outline': {},
     'voxel-voila': {},
     'voxel-keys': {},
 
     // the GUI window (built-in toggle with 'H')
-    'voxel-debug': {},
     'voxel-plugins-ui': {},
     'kb-bindings-ui': {},
     }
