c815332ebb4b8035090f52af60e0935213af4130
deathcap <deathcap@gmx.co.uk>
2015-03-24 22:25:26 -0700

    Pipe through stream to convert to Buffer instead of in workerstream

--- mf-worker.js
+++ mf-worker.js
@@ -2,6 +2,16 @@
 
 var ParentStream = require('workerstream/parent');
 var mineflayer = require('wsmc/mineflayer-stream');
+var through = require('through');
+var toBuffer = require('typedarray-to-buffer');
+var isTypedArray = require('is-typedarray');
+
+// convert Uint8Array from postmessage to nodejs/browserify augmented Buffer
+var toBufferStream = through(function write(data) {
+  if (isTypedArray(data) && !Buffer.isBuffer(data)) data = toBuffer(data);
+  else if (e.data instanceof ArrayBuffer) data = new Buffer(new Uint8Array(data));
+  this.queue(data);
+});
 
 module.exports = function(self) {
   console.log('mf-worker initializing',self);
@@ -11,7 +21,7 @@ module.exports = function(self) {
 
   self.bot = mineflayer.createBot({
     username: 'user1', // TODO
-    stream: self.parentStream,
+    stream: self.parentStream.pipe(toBufferStream),
   });
 
   console.log('mf-worker bot',self.bot);
