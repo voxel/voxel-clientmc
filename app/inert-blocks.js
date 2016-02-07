ba0c92510b05a0475f1078bde56fccae4bfc5f04
deathcap <deathcap@gmx.co.uk>
2016-02-07 14:14:15 -0800

    Move inert block properties to mcblocks.js, data-driven style

--- app/inert-blocks.js
+++ app/inert-blocks.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const mcBlocks = require('../mcblocks');
+
 module.exports = function(game, opts) {
   return new BlocksPlugin(game, opts);
 };
@@ -16,78 +18,12 @@ function BlocksPlugin(game, opts) {
 }
 
 BlocksPlugin.prototype.enable = function() {
-  this.registry.registerBlock('grass', {texture: ['grass_top', 'dirt', 'grass_side'], hardness:1.0, itemDrop: 'dirt', effectiveTool: 'spade'});
-  this.registry.registerBlock('dirt', {texture: 'dirt', hardness:0.75, effectiveTool: 'spade'});
-  this.registry.registerBlock('farmland', {texture: 'farmland_dry'});
-  this.registry.registerBlock('mycelium', {texture: ['mycelium_top', 'dirt', 'mycelium_side']});
-  this.registry.registerBlock('stone', {displayName: 'Smooth Stone', texture: 'stone', hardness:10.0, itemDrop: 'cobblestone', effectiveTool: 'pickaxe', requiredTool: 'pickaxe'});
-  this.registry.registerBlock('waterFlow', {texture: 'water_flow'}); // TODO: animation
-  this.registry.registerBlock('water', {texture: 'water_still'}); // TODO: animation
-  this.registry.registerBlock('lavaFlow', {texture: 'lava_flow'}); // TODO: animation
-  this.registry.registerBlock('lava', {texture: 'lava_still'}); // TODO: animation
-  this.registry.registerBlock('sand', {texture: 'sand'});
-  this.registry.registerBlock('gravel', {texture: 'gravel'});
+  const inertBlockProps = mcBlocks.inertBlockProps;
+  Object.keys(inertBlockProps).forEach((name) => {
+    const props = inertBlockProps[name];
 
-  this.registry.registerBlock('oreGold', {displayName: 'Gold Ore', texture: 'gold_ore', hardness:15.0, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('oreIron', {displayName: 'Iron Ore', texture: 'iron_ore', hardness:15.0, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('oreCoal', {displayName: 'Coal Ore', texture: 'coal_ore', itemDrop: 'coal', hardness:15.0, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('oreLapis', {displayName: 'Lapis Lazuli Ore', texture: 'lapis_ore', hardness:15.0, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('oreDiamond', {displayName: 'Diamond Ore', texture: 'diamond_ore', hardness:15.0, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('oreRedstone', {displayName: 'Redstone Ore', texture: 'redstone_ore', hardness:15.0, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('oreEmerald', {displayName: 'Emerald Ore', texture: 'emerald_ore', hardness:15.0, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('oreNetherQuartz', {displayName: 'Nether Quartz Ore', texture: 'quartz_ore', hardness:15.0, requiredTool: 'pickaxe'});
-
-  this.registry.registerBlock('logOak', {displayName: 'Oak Wood', texture: ['log_oak_top', 'log_oak_top', 'log_oak'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'});
-  this.registry.registerBlock('cobblestone', {texture: 'cobblestone', hardness:10.0, effectiveTool: 'pickaxe', requiredTool: 'pickaxe'});
-  this.registry.registerBlock('brick', {texture: 'brick'});
-  this.registry.registerBlock('leavesOak', {displayName: 'Oak Leaves', texture: 'leaves_oak', transparent: true, hardness: 0.1, creativeTab: 'plants'});
-  this.registry.registerBlock('leavesAcacia', {displayName: 'Acacia Leaves', texture: 'leaves_acacia', transparent: true, hardness: 0.1, creativeTab: 'plants'});
-  this.registry.registerBlock('logBirch', {texture: ['log_birch_top', 'log_birch_top', 'log_birch'], hardness:2.0, displayName: 'Birch Wood', effectiveTool: 'axe', creativeTab: 'plants'});
-  this.registry.registerBlock('logAcacia', {displayName: 'Acacia Wood', texture: ['log_acacia_top', 'log_acacia_top', 'log_acacia'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'});
-
-  this.registry.registerBlock('sponge', {texture: 'sponge'});
-  this.registry.registerBlock('glass', {texture: 'glass', transparent: true, hardness: 0.2});
-  this.registry.registerBlock('blockLapis', {texture: 'lapis_block'});
-  this.registry.registerBlock('sandstone', {texture: 'sandstone_normal'});
-
-  this.registry.registerBlock('wool', {texture: 'wool_colored_white'}); // TODO: metablocks for colors TODO: use voxel-wool..
-
-  this.registry.registerBlock('blockRedstone', {texture: 'redstone_block', displayName: 'Block of Redstone'}); // TODO: move to voxel-decorative?
-  this.registry.registerBlock('blockEmerald', {texture: 'emerald_block', displayName: 'Block of Emerald'}); // TODO: move to voxel-decorative?
-  this.registry.registerBlock('blockQuartz', {texture: 'quartz_block_side', displayName: 'Block of Quartz'}); // TODO: move to voxel-decorative?
-
-  this.registry.registerBlock('tnt', {texture: ['tnt_top', 'tnt_bottom', 'tnt_side']});
-  this.registry.registerBlock('bookshelf', {texture: 'bookshelf'}); // TODO: sides
-  this.registry.registerBlock('stoneMossy', {texture: 'cobblestone_mossy'});
-  this.registry.registerBlock('obsidian', {texture: 'obsidian', hardness: 128, requiredTool: 'pickaxe'});
-  this.registry.registerBlock('snow', {texture: 'snow'});
-  this.registry.registerBlock('ice', {texture: 'ice'});
-  this.registry.registerBlock('cactus', {texture: ['cactus_top', 'cactus_bottom', 'cactus_side']});
-  this.registry.registerBlock('clay', {texture: 'clay'});
-  this.registry.registerBlock('jukebox', {texture: ['jukebox_top', 'planks_oak', 'jukebox_side']});
-  this.registry.registerBlock('netherrack', {texture: 'netherrack'});
-  this.registry.registerBlock('soulsand', {texture: 'soul_sand'});
-  this.registry.registerBlock('glowstone', {texture: 'glowstone'});
-  this.registry.registerBlock('portal', {texture: 'portal'});
-  this.registry.registerBlock('blockMelon', {texture: ['melon_top', 'melon_top', 'melon_side']});
-  this.registry.registerBlock('endstone', {texture: 'end_stone'});
-  this.registry.registerBlock('lampOff', {texture: 'redstone_lamp_off'});
-  this.registry.registerBlock('lampOn', {texture: 'redstone_lamp_on'});
-
-  this.registry.registerBlock('noteblock', {texture: 'noteblock'});
-  this.registry.registerBlock('dispenser', {texture: 'dispenser_front_horizontal'}); // TODO: direction
-  this.registry.registerBlock('dropper', {texture: 'dropper_front_horizontal'}); // TODO: direction
-  this.registry.registerBlock('mushroomBigRed', {texture: 'mushroom_block_skin_red'});
-  this.registry.registerBlock('mushroomBigBrown', {texture: 'mushroom_block_skin_brown'});
-  this.registry.registerBlock('brickNether', {texture: 'nether_brick'});
-  this.registry.registerBlock('endPortalFrame', {texture: ['endframe_top', 'endframe_top', 'endframe_side']});
-  this.registry.registerBlock('command', {texture: 'command_block'});
-  this.registry.registerBlock('clayStainedWhite', {texture: 'hardened_clay_stained_white'});
-  this.registry.registerBlock('clayHardened', {texture: 'hardened_clay'});
-  this.registry.registerBlock('hayBale', {texture: ['hay_block_top', 'hay_block_top', 'hay_block_side']});
-
-  this.registry.registerBlock('missing', {texture: 'no_texture', displayName: 'Missing Block'}); // custom texture (TODO: standard location?)
-  // TODO: more blocks
+    this.registry.registerBlock(name, props);
+  });
 };
 
 BlocksPlugin.prototype.disable = function() {
--- mcblocks.js
+++ mcblocks.js
@@ -109,7 +109,83 @@ const mcBlockName2Voxel = {
   '35:14': 'woolRed',
   '35:15': 'woolBlack',
   */
-}
+};
+
+// Block properties to register for "inert" blocks (those without functionality provided by other plugins yet)
+const inertBlockProps = {
+  grass: {texture: ['grass_top', 'dirt', 'grass_side'], hardness:1.0, itemDrop: 'dirt', effectiveTool: 'spade'},
+  dirt: {texture: 'dirt', hardness:0.75, effectiveTool: 'spade'},
+  farmland: {texture: 'farmland_dry'},
+  mycelium: {texture: ['mycelium_top', 'dirt', 'mycelium_side']},
+  stone: {displayName: 'Smooth Stone', texture: 'stone', hardness:10.0, itemDrop: 'cobblestone', effectiveTool: 'pickaxe', requiredTool: 'pickaxe'},
+  waterFlow: {texture: 'water_flow'}, // TODO: animation
+  water: {texture: 'water_still'}, // TODO: animation
+  lavaFlow: {texture: 'lava_flow'}, // TODO: animation
+  lava: {texture: 'lava_still'}, // TODO: animation
+  sand: {texture: 'sand'},
+  gravel: {texture: 'gravel'},
+
+  oreGold: {displayName: 'Gold Ore', texture: 'gold_ore', hardness:15.0, requiredTool: 'pickaxe'},
+  oreIron: {displayName: 'Iron Ore', texture: 'iron_ore', hardness:15.0, requiredTool: 'pickaxe'},
+  oreCoal: {displayName: 'Coal Ore', texture: 'coal_ore', itemDrop: 'coal', hardness:15.0, requiredTool: 'pickaxe'},
+  oreLapis: {displayName: 'Lapis Lazuli Ore', texture: 'lapis_ore', hardness:15.0, requiredTool: 'pickaxe'},
+  oreDiamond: {displayName: 'Diamond Ore', texture: 'diamond_ore', hardness:15.0, requiredTool: 'pickaxe'},
+  oreRedstone: {displayName: 'Redstone Ore', texture: 'redstone_ore', hardness:15.0, requiredTool: 'pickaxe'},
+  oreEmerald: {displayName: 'Emerald Ore', texture: 'emerald_ore', hardness:15.0, requiredTool: 'pickaxe'},
+  oreNetherQuartz: {displayName: 'Nether Quartz Ore', texture: 'quartz_ore', hardness:15.0, requiredTool: 'pickaxe'},
+
+  logOak: {displayName: 'Oak Wood', texture: ['log_oak_top', 'log_oak_top', 'log_oak'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'},
+  cobblestone: {texture: 'cobblestone', hardness:10.0, effectiveTool: 'pickaxe', requiredTool: 'pickaxe'},
+  brick: {texture: 'brick'},
+  leavesOak: {displayName: 'Oak Leaves', texture: 'leaves_oak', transparent: true, hardness: 0.1, creativeTab: 'plants'},
+  leavesAcacia: {displayName: 'Acacia Leaves', texture: 'leaves_acacia', transparent: true, hardness: 0.1, creativeTab: 'plants'},
+  logBirch: {texture: ['log_birch_top', 'log_birch_top', 'log_birch'], hardness:2.0, displayName: 'Birch Wood', effectiveTool: 'axe', creativeTab: 'plants'},
+  logAcacia: {displayName: 'Acacia Wood', texture: ['log_acacia_top', 'log_acacia_top', 'log_acacia'], hardness:2.0, effectiveTool: 'axe', creativeTab: 'plants'},
+
+  sponge: {texture: 'sponge'},
+  glass: {texture: 'glass', transparent: true, hardness: 0.2},
+  blockLapis: {texture: 'lapis_block'},
+  sandstone: {texture: 'sandstone_normal'},
+
+  wool: {texture: 'wool_colored_white'}, // TODO: metablocks for colors TODO: use voxel-wool..
+
+  blockRedstone: {texture: 'redstone_block', displayName: 'Block of Redstone'}, // TODO: move to voxel-decorative?
+  blockEmerald: {texture: 'emerald_block', displayName: 'Block of Emerald'}, // TODO: move to voxel-decorative?
+  blockQuartz: {texture: 'quartz_block_side', displayName: 'Block of Quartz'}, // TODO: move to voxel-decorative?
+
+  tnt: {texture: ['tnt_top', 'tnt_bottom', 'tnt_side']},
+  bookshelf: {texture: 'bookshelf'}, // TODO: sides
+  stoneMossy: {texture: 'cobblestone_mossy'},
+  obsidian: {texture: 'obsidian', hardness: 128, requiredTool: 'pickaxe'},
+  snow: {texture: 'snow'},
+  ice: {texture: 'ice'},
+  cactus: {texture: ['cactus_top', 'cactus_bottom', 'cactus_side']},
+  clay: {texture: 'clay'},
+  jukebox: {texture: ['jukebox_top', 'planks_oak', 'jukebox_side']},
+  netherrack: {texture: 'netherrack'},
+  soulsand: {texture: 'soul_sand'},
+  glowstone: {texture: 'glowstone'},
+  portal: {texture: 'portal'},
+  blockMelon: {texture: ['melon_top', 'melon_top', 'melon_side']},
+  endstone: {texture: 'end_stone'},
+  lampOff: {texture: 'redstone_lamp_off'},
+  lampOn: {texture: 'redstone_lamp_on'},
+
+  noteblock: {texture: 'noteblock'},
+  dispenser: {texture: 'dispenser_front_horizontal'}, // TODO: direction
+  dropper: {texture: 'dropper_front_horizontal'}, // TODO: direction
+  mushroomBigRed: {texture: 'mushroom_block_skin_red'},
+  mushroomBigBrown: {texture: 'mushroom_block_skin_brown'},
+  brickNether: {texture: 'nether_brick'},
+  endPortalFrame: {texture: ['endframe_top', 'endframe_top', 'endframe_side']},
+  command: {texture: 'command_block'},
+  clayStainedWhite: {texture: 'hardened_clay_stained_white'},
+  clayHardened: {texture: 'hardened_clay'},
+  hayBale: {texture: ['hay_block_top', 'hay_block_top', 'hay_block_side']},
+
+  missing: {texture: 'no_texture', displayName: 'Missing Block'}, // custom texture (TODO: standard location?)
+  // TODO: more blocks
+};
 
 const mcBlockID2Voxel = {
   default: 'missing'
@@ -126,4 +202,5 @@ Object.keys(mcBlockName2Voxel).forEach((mcName) => {
 module.exports = {
   mcBlockName2Voxel,
   mcBlockID2Voxel,
+  inertBlockProps,
 };
